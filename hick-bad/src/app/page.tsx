d1f841bf2587296b780afb869f8810e654e87189
Mai Vang <vmaineng@gmail.com>
2025-09-09 18:30:41 -0700

    updated hicks

--- hick-bad/src/app/page.tsx
+++ hick-bad/src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-
+import Navbar from "./components/Navbar";
 import Beigecolors from "./components/Beigecolors";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Beigecolors />
     </div>
   );
