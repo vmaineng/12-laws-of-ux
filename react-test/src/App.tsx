d6e8d9face38b73407a73f88bc83690ec7e49cbd
Mai Vang <vmaineng@gmail.com>
2025-09-10 14:00:10 -0700

    added in react test

---
+++ react-test/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+declare global {
+  interface Window {
+    timestamp: number;
+  }
+}
+
+export default function App() {
+  const [state] = useState(0);
+  console.log(1);
+
+  const start = Date.now();
+  while (Date.now() - start < 50) {
+    window.timestamp = Date.now();
+  }
+
+  useEffect(() => {
+    console.log(2);
+  }, [state]);
+
+  Promise.resolve().then(() => console.log(3));
+
+  setTimeout(() => console.log(4), 0);
+
+  return null;
+}
